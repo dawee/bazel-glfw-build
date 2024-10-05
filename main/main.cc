752bc55c1eac70d091ef42f98ab35b4c2874c2f7
David Corticchiato <david.corticchiato@gmail.com>
2024-10-05 18:31:17 -0400

    build with assimp

--- main/main.cc
+++ main/main.cc
@@ -1,5 +1,9 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
 
 #include <iostream>
 
@@ -22,6 +26,32 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "{\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
+
+bool DoTheImportThing( const std::string& pFile) {
+  // Create an instance of the Importer class
+  Assimp::Importer importer;
+
+  // And have it read the given file with some example postprocessing
+  // Usually - if speed is not the most important aspect for you - you'll
+  // probably to request more postprocessing than we do in this example.
+  const aiScene* scene = importer.ReadFile( pFile,
+    aiProcess_CalcTangentSpace       |
+    aiProcess_Triangulate            |
+    aiProcess_JoinIdenticalVertices  |
+    aiProcess_SortByPType);
+
+  // If the import failed, report it
+  if (nullptr == scene) {
+    // DoTheErrorLogging( importer.GetErrorString());
+    return false;
+  }
+
+  // Now we can access the file's contents.
+//   DoTheSceneProcessing( scene);
+
+  // We're done. Everything will be cleaned up by the importer destructor
+  return true;
+}
 
 int main()
 {
